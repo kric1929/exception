0ab715e16d9262f5f4a3671df3f76ea8f81456bf
Mihail Zharkov <kric19299@gmail.com>
2020-01-22 13:00:41 +0300

    Improved exceptions

--- notation.py
+++ notation.py
@@ -3,6 +3,7 @@ number_1 = number_1.split()
 
 assert (number_1[0] == '+' or number_1[0] == '-' or number_1[0] == '*' or number_1[0] == '/'), 'Недоступная операция'
 
+
 try:
     if number_1[0] == '+':
         print(int(number_1[1]) + int(number_1[2]))
@@ -11,6 +12,13 @@ try:
     elif number_1[0] == '*':
         print(int(number_1[1]) * int(number_1[2]))
     elif number_1[0] == '/':
-        print(int(number_1[1]) / int(number_1[2]))
-except Exception as e:
-    print(e)
+        try:
+            print(int(number_1[1]) / int(number_1[2]))
+        except ValueError:
+            print('Строки делить нельзя!')
+except ZeroDivisionError:
+    print('На ноль делить нельзя!')
+except IndexError:
+    print('Передано неправильное количество аргументов!')
+
+print('Выполнено')
